f5c4ff938d18e73b985876e27adfe33d9f30b3ad
RaphaelaMulthaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2024-04-10 06:28:45 +0200

    assigned to css

--- js/addTask.js
+++ js/addTask.js
@@ -2,9 +2,13 @@ function toggleDropdown() {
     var dropdown = document.getElementById('addTaskAssignedTo');
     dropdown.classList.toggle('active');
     var arrowIcon = document.getElementById('arrowIcon');
-    arrowIcon.classList.toggle('down');
-    arrowIcon.classList.toggle('up');
+    if (arrowIcon.src.includes("Down")) {
+        arrowIcon.src = "./assets/img/arrowdropdownup.svg";
+    } else {
+        arrowIcon.src = "./assets/img/arrowdropdownDown.svg";
+    }
 }
+
 
 document.addEventListener("DOMContentLoaded", function() {
     var divAddTaskSubtasks = document.querySelector(".divAddTaskSubtasks");
