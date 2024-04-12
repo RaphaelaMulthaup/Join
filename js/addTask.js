7a4157c8cc52f2b3071985daed45ba3f5ea81e7d
RaphaelaMulthaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2024-04-12 12:24:44 +0200

    focus input and textarea add task and form validation

--- js/addTask.js
+++ js/addTask.js
@@ -12,7 +12,6 @@ function toggleDropdown() {
     }
 }
 
-
 /**
  * This eventlistener adds a plus sign inside a circular div to the Subtasks form field.
  */
@@ -22,6 +21,41 @@ document.addEventListener("DOMContentLoaded", function() {
     divAddTaskSubtasks.innerHTML += dataAddContent;
 });
 
-function priorityUrgent(){
+/**
+ * This function disables validation of the form, styles the button with urgent prioryty prominently and removes the prominent style from the other two buttons
+ */
+function priorityUrgent(event){
+    event.preventDefault();
     document.getElementById('addTaskPrioMedium').classList.remove('addTaskPrioActiv', 'addTaskPrioMediumActiv');
-    document.getElementById('imgMedium').src = './assets/img/capaMedium.svg';}
+    document.getElementById('imgMedium').src = './assets/img/capaMedium.svg';
+    document.getElementById('addTaskPrioLow').classList.remove('addTaskPrioActiv', 'addTaskPrioLowActiv');
+    document.getElementById('imgLow').src = './assets/img/capaLow.svg';
+    document.getElementById('addTaskPrioUrgent').classList.add('addTaskPrioActiv', 'addTaskPrioUrgentActiv');
+    document.getElementById('imgUrgent').src = './assets/img/capaUrgentWhite.svg';
+}
+
+/**
+ * This function disables validation of the form, styles the button with medium prioryty prominently and removes the prominent style from the other two buttons
+ */
+function priorityMedium(event){
+    event.preventDefault();
+    document.getElementById('addTaskPrioUrgent').classList.remove('addTaskPrioActiv', 'addTaskPrioUrgentActiv');
+    document.getElementById('imgUrgent').src = './assets/img/capaUrgent.svg';
+    document.getElementById('addTaskPrioLow').classList.remove('addTaskPrioActiv', 'addTaskPrioLowActiv');
+    document.getElementById('imgLow').src = './assets/img/capaLow.svg';
+    document.getElementById('addTaskPrioMedium').classList.add('addTaskPrioActiv', 'addTaskPrioMediumActiv');
+    document.getElementById('imgMedium').src = './assets/img/capaMediumWhite.svg';
+}
+
+/**
+ *This function disables validation of the form, styles the button with low prioryty prominently and removes the prominent style from the other two buttons
+ */
+function priorityLow(event){
+    event.preventDefault();
+    document.getElementById('addTaskPrioMedium').classList.remove('addTaskPrioActiv', 'addTaskPrioMediumActiv');
+    document.getElementById('imgMedium').src = './assets/img/capaMedium.svg';
+    document.getElementById('addTaskPrioUrgent').classList.remove('addTaskPrioActiv', 'addTaskPrioUrgentActiv');
+    document.getElementById('imgUrgent').src = './assets/img/capaUrgent.svg';
+    document.getElementById('addTaskPrioLow').classList.add('addTaskPrioActiv', 'addTaskPrioLowActiv');
+    document.getElementById('imgLow').src = './assets/img/capaLowWhite.svg';
+}
