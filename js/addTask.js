36325e2fd2afd9c034b0541d942ac91c05f0c925
RaphaelaMulthaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2024-04-13 15:45:24 +0200

    form validation add task

--- js/addTask.js
+++ js/addTask.js
@@ -59,3 +59,54 @@ function priorityLow(event){
     document.getElementById('addTaskPrioLow').classList.add('addTaskPrioActiv', 'addTaskPrioLowActiv');
     document.getElementById('imgLow').src = './assets/img/capaLowWhite.svg';
 }
+
+/**
+ * This event listener checks validity. If the value is valid, the error display and red border are removed, if applicable.
+ */
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll('.addTaskInputRequired').forEach(inputElement => {
+        inputElement.addEventListener("input", function() {
+            if (inputElement.checkValidity()) {
+                // Wenn das Eingabefeld gültig ist, entfernen Sie die Fehlermeldung und den roten Rahmen
+                let errorMessageElement = document.getElementById("requiredMessage" + inputElement.id.replace("addTask", "") + "AddTask");
+                errorMessageElement.style.visibility = 'hidden';
+                inputElement.classList.remove("borderRed");
+            }
+        });
+    });
+});
+
+/**
+ * This function checks whether the value of the input field is valid. In this case, an error message is displayed and the border of the input field turns red.
+ * 
+ * @param {string} element The element that is to be checked is named.
+ */
+function validateInput(element) {
+    let inputElement = document.getElementById("addTask" + element);
+    let errorMessageElement = document.getElementById("requiredMessage" + element + "AddTask");
+    
+    if (!inputElement.checkValidity()) {
+        // Benutzerdefinierte Fehlermeldung anzeigen
+        errorMessageElement.style.visibility = 'visible';
+        inputElement.classList.add("borderRed"); // Färben Sie den Rahmen des Eingabefelds rot
+    }
+}
+
+/**
+ * This function checks whether the required input fields are valid and submits the form if necessary.
+ */
+function addNewTask() {
+    validateInput("Title");
+    validateInput("Description");
+    validateInput("DueDate");
+    validateInput("Category");
+
+    let titleInput = document.getElementById("addTaskTitle");
+    let descriptionInput = document.getElementById("addTaskDescription");
+    let dueDateInput = document.getElementById("addTaskDueDate");
+    let categoryInput = document.getElementById("addTaskCategory");
+      
+    if (titleInput.checkValidity() && descriptionInput.checkValidity() && dueDateInput.checkValidity() && categoryInput.checkValidity()) {
+        document.getElementById("formAddNewTask").submit();
+    } 
+}
