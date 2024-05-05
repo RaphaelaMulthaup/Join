bd79d7d14bd035a30b823212256ce47a34a69c58
RaphaelaMulthaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2024-05-05 11:35:43 +0200

    add task overlay

--- js/board.js
+++ js/board.js
@@ -8,8 +8,10 @@ function setElementsToDefaultBoard(){
     findTaskDefault();
 }
 
+/*headline*/
+
 /**
- * This function colors the border of 'findTask' blue and puts a focus on the input field.  Beforehand, it is checked whether something else is open or activated that should be closed beforehand.
+ * This function colors the border of 'findTask' blue and puts a focus on the input field. Beforehand, it is checked whether something else is open or activated that should be closed beforehand.
  */
 function findTaskActive(){
     if (subMenuOpen) {
@@ -29,4 +31,19 @@ function findTaskDefault(){
     let findTask = document.getElementById('findTask');
     findTask.style.border = "1px solid #A8A8A8";
     findTaskFocus = false;
+}
+
+function plusButtonToDefault(plusButton) {
+    let rect = plusButton.querySelector('rect');
+    let paths = plusButton.querySelectorAll('path');
+    setTimeout(function() {
+        rect.setAttribute('stroke', '#2A3647');
+        paths.forEach(function(path) {
+            path.setAttribute('stroke', '#2A3647');
+        });
+    }, 300);
+}
+
+function openOverlayAddTask(){
+    document.getElementById('overlayAddTaskBackground').classList.remove('dNone');
 }
