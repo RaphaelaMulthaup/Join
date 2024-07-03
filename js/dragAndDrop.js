5dfa2f831ddb0a91cb07602f6d251b38d07ac1cc
RaphaelaMulthaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2024-07-03 14:59:32 +0200

    Revert "stash clearing"
    This reverts commit d247d9c059aac26b2764b8e9abd663be38afda45.

--- js/dragAndDrop.js
+++ js/dragAndDrop.js
@@ -1,39 +1,54 @@
 let currentDraggedElement;
 
 function updateHTML(){
-    let tasksToDo = tasks.filter(t => t['status'] == 'to do');
+
+    let tasksToDo = tasks.filter(t => t['status'] == 'tasksToDo');
 
     document.getElementById('tasksToDo').innerHTML = '';
 
     for (let index = 0; index < tasksToDo.length; index++) {
         const element = tasksToDo[index];
-        document.getElementById('tasksToDo').innerHTML += displayMiniCard(element);
+        document.getElementById('tasksToDo').innerHTML += htmlboard(element);
     }
-    let tasksInProgress = tasks.filter(t => t['status'] == 'in progress');
+    
+    let tasksInProgress = tasks.filter(t => t['status'] == 'tasksInProgress');
 
     document.getElementById('tasksInProgress').innerHTML = '';
 
     for (let index = 0; index < tasksInProgress.length; index++) {
         const element = tasksInProgress[index];
-        document.getElementById('tasksInProgress').innerHTML += displayMiniCard(element);
+        document.getElementById('tasksInProgress').innerHTML += htmlboard(element);
     }
-    let tasksAwaitFeedback = tasks.filter(t => t['status'] == 'await feedback');
+    let tasksAwaitFeedback = tasks.filter(t => t['status'] == 'tasksAwaitFeedback');
 
     document.getElementById('tasksAwaitFeedback').innerHTML = '';
 
     for (let index = 0; index < tasksAwaitFeedback.length; index++) {
         const element = tasksAwaitFeedback[index];
-        document.getElementById('tasksAwaitFeedback').innerHTML += displayMiniCard(element);
+        document.getElementById('tasksAwaitFeedback').innerHTML += htmlboard(element);
     }
-    let tasksDone = tasks.filter(t => t['status'] == 'done');
+    let tasksDone = tasks.filter(t => t['status'] == 'tasksDone');
 
     document.getElementById('tasksDone').innerHTML = '';
 
     for (let index = 0; index < tasksDone.length; index++) {
         const element = tasksDone[index];
-        document.getElementById('tasksDone').innerHTML += displayMiniCard(element);
+        document.getElementById('tasksDone').innerHTML += htmlboard(element);
     }
 }
+
+// function updateHTML(){
+//     let taskStatuses = ['tasksToDo', 'tasksInProgress', 'tasksAwaitFeedback', 'tasksDone'];
+//     taskStatuses.forEach(status => {
+//         let tasksByStatus = tasks.filter(t => t.status == status);
+//         document.getElementById(status).innerHTML = '';
+
+//         for (let index = 0; index < tasksByStatus.length; index++) {
+//             const element = tasksByStatus[index];
+//             document.getElementById(status).innerHTML += htmlMiniCard(element, index);
+//         }
+//     });
+// }
 
 function startDragging(id) {
     currentDraggedElement = id;
@@ -43,20 +58,19 @@ function allowDrop(ev) {
     ev.preventDefault();
 }
 
-async function moveTo(status) {
-    tasks[currentDraggedElement]['status'] = status;
+function moveTo(ev, status) {
     console.log(status);
     // ev.preventDefault();
     tasks[currentDraggedElement].status = status;
     console.log('tasks', tasks);
-    await putTasksToDatabase(tasks);
-    displayBoard();
+    putData(tasks);
+    updateHTML();
 }
 
-function highlight(i) {
-    document.getElementById(i).classList.add('drag-area-highlight');
+function highlight(id) {
+    document.getElementById(id).classList.add('drag-area-highlight');
 }
 
-function removeHighlight(i) {
-    document.getElementById(i).classList.remove('drag-area-highlight');
+function removeHighlight(id) {
+    document.getElementById(id).classList.remove('drag-area-highlight');
 }
